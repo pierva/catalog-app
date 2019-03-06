492950e4eaaf6cc14209e7da177b4574a18f5201
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-03-06 18:48:37 -0500

    feat: Add js code for facebook login

--- static/js/login.js
+++ static/js/login.js
@@ -26,3 +26,57 @@ function signInCallback(authResult) {
     });
   }
 }
+
+// Facebook login
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : 'YOUR-APP-ID',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v3.2'
+  });
+
+  FB.AppEvents.logPageView();
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+// Load the FB SDK asynchronously
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+function sendTokenToServer() {
+  var access_token = FB.getAuthResponse()['accessToken'];
+  console.log(access_token)
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    $.ajax({
+      type: 'POST',
+      url: '/fbconnect?state={{STATE}}',
+      processData: false,
+      data: access_token,
+      contentType: 'application/octet-stream; charset=utf-8',
+      success: function(result) {
+       // Handle or verify the server response if necessary.
+      if (result) {
+        $('#result').html('Login Successful!</br>'+ result + '</br>Redirecting...')
+        setTimeout(function() {
+          window.location.href = "/catalog";
+        }, 4000);
+
+        } else {
+          $('#result').html('Failed to make a server-side call. Check your configuration and console.');
+        }
+      }
+    });
+  });
+}
