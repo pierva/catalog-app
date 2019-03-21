c9ba161b3f89aea3e669b51ce401a3d9c2cd2847
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-03-21 17:02:04 -0400

    chore: Add configuration for production

--- catalog/config.py
+++ catalog/config.py
@@ -1,4 +1,13 @@
+# catalog/config.py
+
 import os
+try:
+    # Python 2.7
+    import ConfigParser as configparser
+except ImportError:
+    # Python 3
+    import configparser
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -38,3 +47,18 @@ class ProductionConfig(BaseConfig):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/pierva'
     DEBUG_TB_ENABLED = False
+
+    # production config takes precedence over env variables
+
+    # production config file at ./catalog/instance/production.cfg
+    config_path = os.path.join(basedir, 'instance', 'production.cfg')
+
+    # if config file exists, read it:
+    if os.path.isfile(config_path):
+        config = configparser.ConfigParser()
+
+        with open(config_path) as configfile:
+            config.readfp(configfile)
+
+        SECRET_KEY = config.get('keys', 'SECRET_KEY')
+        SECURITY_PASSWORD_SALT = config.get('keys', 'SECURITY_PASSWORD_SALT')
