d81d17efe590991ee506f73e8f56a981bf24bdd8
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-03-23 22:25:56 -0400

    fix: Update parent-child relationship errros

--- catalog/models.py
+++ catalog/models.py
@@ -13,10 +13,11 @@ class User(db.Model):
     password = db.Column(db.String(64), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
+    categories = db.relationship("Category", lazy=True, back_populates='user')
 
     def __init__(self, email, password, admin, username=""):
         self.email = email
-        self.password = bcrypt.generate_password_hash(password)
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
         self.registered_on = datetime.datetime.now()
         self.admin = admin
         self.username = username
@@ -37,15 +38,39 @@ class User(db.Model):
         return '<email {}'.format(self.email)
 
 
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", back_populates='categories',
+                           cascade="save-update")
+    items = db.relationship('Item', back_populates='category',
+                            cascade="all, delete")
+
+    @property
+    def serialize_category(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
 class Item(db.Model):
     __tablename__ = 'item'
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(50), nullable=False)
     picture = db.Column(db.String, nullable=True)
     description = db.Column(db.String)
-    category_id = db.Column(db.Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
+                            nullable=False)
     category_name = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('category.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                        nullable=False)
+    category = db.relationship("Category", foreign_keys=[category_id],
+                               back_populates="items", cascade="all, delete")
+    db.relationship("User")
 
 
     @property
@@ -58,22 +83,4 @@ class Item(db.Model):
             'description': self.description,
             'category_id': self.category_id,
             'category_name': self.category_name,
-        }
-
-
-class Category(db.Model):
-    __tablename__ = 'category'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    db.relationship(User)
-    db.relationship(Item, cascade="all, delete")
-
-    @property
-    def serialize_category(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'user_id': self.user_id
         }
